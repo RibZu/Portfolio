8d891e9afd9024297e679b50ee8d9c8032b3f61a
RibZu <109860001+RibZu@users.noreply.github.com>
2026-09-21 00:26:44 -0300

    Fix build: absolute URLs for alternate links and valid encoding

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        en: resolve(__dirname, 'en/index.html')
+        main: 'index.html',
+        en: 'en/index.html'
       }
     }
   }
