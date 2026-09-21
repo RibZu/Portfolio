1dc8515b38d3916285b6a15de5d5841ff3eb56fe
RibZu <109860001+RibZu@users.noreply.github.com>
2026-09-21 00:31:14 -0300

    Conectar componentes al archivo App.jsx principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import { Projects } from './components/Projects';
+import Background from './components/Background';
+import Contact from './components/Contact';
+
 export default function App() {
   return (
-    <div className="container">
-      <header id="header">
-        {/* Header will be implemented in Phase 3 */}
-      </header>
-      
+    <>
+      <Header />
       <main>
-        <section id="skills" aria-labelledby="skills-heading">
-          {/* Skills will be implemented in Phase 3 */}
-        </section>
-        
-        <section id="projects" aria-labelledby="projects-heading">
-          {/* Projects will be implemented in Phase 4 */}
-        </section>
-        
-        <section id="background" aria-labelledby="background-heading">
-          {/* Background will be implemented in Phase 5 */}
-        </section>
-        
-        <section id="contact" aria-labelledby="contact-heading">
-          {/* Contact will be implemented in Phase 6 */}
-        </section>
+        <Hero />
+        <Skills />
+        <Projects />
+        <Background />
+        <Contact />
       </main>
-    </div>
+    </>
   );
 }
